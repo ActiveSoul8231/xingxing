6aadd0a3a3e52abe19b0e1edf5d74982eac1f397
xmsh89 <57211091+xmsh89@users.noreply.github.com>
2019-11-05 16:05:06 +0800

    33

--- src/main/java/com/ssm/xingxingsystem/service/PhotoService.java
+++ src/main/java/com/ssm/xingxingsystem/service/PhotoService.java
@@ -1,4 +1,16 @@
 package com.ssm.xingxingsystem.service;
-
+import com.ssm.xingxingsystem.bean.Photo;
+import com.ssm.xingxingsystem.util.PageCountUtil;
+import java.util.List;
 public interface PhotoService {
+    Integer getTableCount();
+
+    List<Photo> getPhotoList(PageCountUtil pageCountUtil, String beginTime, String endTime, String fileName);
+
+
+    void insertPhoto(Photo photo);
+
+    Photo getPhotoById(Integer id);
+
+    void deletePhoto(Integer id);
 }
--- src/main/java/com/ssm/xingxingsystem/service/impl/PhotoServiceImpl.java
+++ src/main/java/com/ssm/xingxingsystem/service/impl/PhotoServiceImpl.java
@@ -1,4 +1,43 @@
 package com.ssm.xingxingsystem.service.impl;
+import com.ssm.xingxingsystem.bean.Photo;
+import com.ssm.xingxingsystem.dao.PhotoDao;
+import com.ssm.xingxingsystem.service.PhotoService;
+import com.ssm.xingxingsystem.util.PageCountUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public class PhotoServiceImpl {
+import java.util.List;
+//事务注解
+@Service
+@Transactional
+public class PhotoServiceImpl implements PhotoService{
+    @Autowired
+    private PhotoDao photoDao;
+    @Override
+    public Integer getTableCount() {
+        return photoDao.getTableCount();
+    }
+
+    @Override
+    public List<Photo> getPhotoList(PageCountUtil pageCountUtil, String beginTime, String endTime, String fileName) {
+        return photoDao.getPhotoList(pageCountUtil,beginTime,endTime,fileName);
+    }
+
+    @Override
+    public void insertPhoto(Photo photo) {
+        photoDao.insertPhoto(photo);
+    }
+
+    @Override
+    public Photo getPhotoById(Integer id) {
+        return photoDao.getPhotoById(id);
+    }
+
+    @Override
+    public void deletePhoto(Integer id) {
+        photoDao.deletePhoto(id);
+    }
+
+
 }
